c9c04e47cace5b1233f17f0342c796cd98255ac0
Samir <ps8949409@gmail.com>
2025-09-29 11:08:04 +0530

    commited

--- lab_manual/Q15.java
+++ lab_manual/Q15.java
@@ -1,13 +1,17 @@
 class Q15{
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++){
-            for(int k=1;k<=5-i;k++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print(i+" ");
-            }
-            System.out.println();
-        }
+        // for(int i=1;i<=5;i++){
+        // // print first
+        //     for(int k=1;k<=5-i;k++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(i+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        
     }
 }
