89f936c564af537efbd0e206b09d80b103e03cff
Samir <ps8949409@gmail.com>
2025-10-06 10:13:16 +0530

    06-10-2025

---
+++ Fact_Recus.java
@@ -0,0 +1,13 @@
+public class Fact_Recus {
+    static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        int ans=fact(5);
+        System.out.println(ans);
+    }
+    
+}
