acf1d73c40ecc5fdd262aa59aa3a1d80771bf8e7
Samir <ps8949409@gmail.com>
2025-10-08 09:54:52 +0530

    08-10-2025

--- lab_manual/Q15.java
+++ lab_manual/Q15.java
@@ -1,6 +1,18 @@
+import java.util.*;
 class Q15 {
     
     public static void main(String[] args) {    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter valu : ");
+        float valu=sc.nextFloat();
+        System.out.print("Intrest rate : ");
+        float intrest=sc.nextFloat();
+        System.out.print("Duration : ");
+        float dua=sc.nextFloat();
+
+        float ans=(valu*intrest*dua)/100;
         
+        System.out.println("total intrest is : "+ ans);
     }
 }
