1c141dceb469148bf0cbbae847fffd034b10eafe
Samir <ps8949409@gmail.com>
2025-10-10 12:07:44 +0530

    10-10-2025

---
+++ StaticMethod.java
@@ -0,0 +1,10 @@
+class School{
+    static void ring(){
+        System.out.println("ring ");
+    }
+}
+public class StaticMethod {
+    public static void main(String[] args) {
+        ring();
+    }
+}
