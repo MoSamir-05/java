276d06b530539b4b4fe396c5a4030ba8e3ad339c
Samir <ps8949409@gmail.com>
2025-11-10 11:00:34 +0530

    10-11-2025

---
+++ lab_manual/Q21.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class std{
+    static void std_input(int total){
+        for (int i=1;i>=total;i++){
+
+        }
+    }
+}
+public class Q21 {
+    public static void main(String[] args) {
+        
+    }
+}
