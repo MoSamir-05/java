7ad1b2da28ac5d0b4ea204b6d7f033e06deb152d
Samir <ps8949409@gmail.com>
2025-11-10 10:41:44 +0530

    10-11-2025

---
+++ Enum_class.java
@@ -0,0 +1,9 @@
+enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+public class Enum_class {
+    public static void main(String[] args) {
+        Day d = Day.MONDAY;
+        System.out.println(d);
+    }
+}
