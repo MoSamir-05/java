ae75459b062d8fe3036286576da65d7194e2fb24
Samir <ps8949409@gmail.com>
2025-12-08 10:10:14 +0530

    08-12-2025

---
+++ lab_manual/Q3/Q1.java
@@ -0,0 +1,8 @@
+import pack.a;
+
+public class Q1 {
+    public static void main(String[] args) {
+        a a1 = new a();
+        a1.show("samir");
+    }
+}
---
+++ lab_manual/Q3/pack/a.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class a {
+    public void show(String a){
+        System.out.println("Pacage method called");
+        System.out.println("Your String is "+a);
+    }
+}
